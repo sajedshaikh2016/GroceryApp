9efbd422bc9ac3003fccd8699653b1ff97be0787
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-10-05 20:02:06 +0530

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Sajed Shaikh on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
